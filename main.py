d48215de8566c36ca5345625b33240aaa2cfebfc
WebCresson <alexis@webcresson.com>
2023-10-20 15:10:48 +0200

    Delete_node

--- main.py
+++ main.py
@@ -1,61 +1,80 @@
 import pandas as pd
 import re
-from tqdm import tqdm  # Importation de tqdm
+from tqdm import tqdm
+import openpyxl
 
 # Fonction pour vérifier si une valeur est une heure
 def is_hour(value):
     return bool(re.match(r"^[0-2]?[0-9]H[0-5][0-9]$", value))
 
-# Fonction pour extraire le code Regate et l'établissement de la chaîne combinée
-def extract_regate_and_establishment(val):
-    regate_code = val.split()[0] if val and val[0].isdigit() else None
-    establishment = " ".join(val.split()[1:]) if regate_code else None
-    return regate_code, establishment
+# Fonction pour générer des noms de colonnes dynamiques
+def generate_column_names(role, count):
+    suffix = f" {count}" if count > 1 else ""
+    return [f"Code Regate ({role}{suffix})", f"Etablissement ({role}{suffix})", f"Heure_1 ({role}{suffix})", f"Heure_2 ({role}{suffix})"]
 
-# Lire le fichier brut CSV
-file_path = "brut_rule.csv"
-data = pd.read_csv(file_path, sep=',', encoding='utf-8')  # Assurez-vous d'utiliser le bon délimiteur et encodage
-
-# Mappage des rôles aux colonnes correspondantes
-role_columns = {
-    "Dépôt": ["Code Regate (Dépôt)", "Etablissement (Dépôt)", "Heure_1 (Dépôt)", "Heure_2 (Dépôt)"],
-    "Traitement": ["Code Regate (Traitement)", "Etablissement (Traitement)", "Heure_1 (Traitement)", "Heure_2 (Traitement)"],
-    "Dépôt et Traitement": ["Code Regate (Dépôt et Traitement)", "Etablissement (Dépôt et Traitement)", "Heure_1 (Dépôt et Traitement)", "Heure_2 (Dépôt et Traitement)"]
-}
-
-structured_data = []
-
-# Itérer sur les numéros de contrat uniques en utilisant tqdm pour afficher la barre de progression
-for contract in tqdm(data["no_contr"].unique(), desc="Traitement des contrats", ncols=100):
-    contract_rows = data[data["no_contr"] == contract]
-    row_data = {
-        "no_contr": contract,
-        "no_ver": contract_rows["no_ver"].iloc[0],
-        "lb_srvce": contract_rows["lb_srvce"].iloc[0]
+# Fonction principale pour traiter les données du contrat
+def process_contract_rows(rows):
+    data = {
+        "no_contr": rows.iloc[0]["no_contr"],
+        "no_ver": rows.iloc[0]["no_ver"],
+        "lb_srvce": rows.iloc[0]["lb_srvce"]
     }
     
-    # Traiter les valeurs en fonction du rôle
-    for role, columns in role_columns.items():
-        role_row_idx = contract_rows.index[contract_rows["val_cri"] == role].tolist()
-        if not role_row_idx:
-            continue
-        role_row_idx = role_row_idx[0]
-        
-        role_related_rows = contract_rows[contract_rows.index < role_row_idx]["val_cri"].tolist()
-        
-        if role_related_rows:
-            regate_code, establishment = extract_regate_and_establishment(role_related_rows[0])
-            row_data[columns[0]] = regate_code
-            row_data[columns[1]] = establishment
-        
-            hour_values = [val for val in role_related_rows[1:] if is_hour(val)]
-            for i, col in enumerate(columns[2:], start=0):
-                row_data[col] = hour_values[i] if len(hour_values) > i else None
-            
-    structured_data.append(row_data)
+    role_counts = {"Dépôt": 0, "Traitement": 0, "Dépôt et Traitement": 0}
 
-# Convertir les données structurées en un DataFrame
-result = pd.DataFrame(structured_data)
+    i = 0
+    while i < len(rows):
+        row = rows.iloc[i]
+        if row["val_cri"] in role_counts:
+            role_counts[row["val_cri"]] += 1
+            columns = generate_column_names(row["val_cri"], role_counts[row["val_cri"]])
 
-# Enregistrer le DataFrame dans un fichier CSV
-result.to_csv("structured_rule.csv", sep=',', encoding='utf-8', index=False)  # Utilisez le même délimiteur et encodage que pour la lecture
+            if i - 1 >= 0 and is_hour(rows.iloc[i-1]["val_cri"]):
+                data[columns[2]] = rows.iloc[i-1]["val_cri"]
+                if i - 2 >= 0 and is_hour(rows.iloc[i - 2]["val_cri"]):
+                    data[columns[3]] = rows.iloc[i-2]["val_cri"]
+                    if i - 3 >= 0:
+                        establishment_info = rows.iloc[i-3]["val_cri"].split(maxsplit=1)
+                    else:
+                        establishment_info = [None, None]
+                else:
+                    data[columns[3]] = None
+                    if i - 2 >= 0:
+                        establishment_info = rows.iloc[i-2]["val_cri"].split(maxsplit=1)
+                    else:
+                        establishment_info = [None, None]
+            else:
+                data[columns[2]] = None
+                data[columns[3]] = None
+                if i - 1 >= 0:
+                    establishment_info = rows.iloc[i-1]["val_cri"].split(maxsplit=1)
+                else:
+                    establishment_info = [None, None]
+
+            code_regate = establishment_info[0]
+            establishment = establishment_info[1] if len(establishment_info) > 1 else None
+
+            data[columns[0]] = code_regate
+            data[columns[1]] = establishment
+        i += 1
+    return data
+
+# Lire les données CSV
+file_path = "brut_rule.csv"
+df = pd.read_csv(file_path, sep=',', encoding='utf-8')
+
+# Préparation pour le traitement des données
+result_data = []
+contracts = df["no_contr"].unique()
+
+# Traiter chaque contrat
+for contract in tqdm(contracts, desc="Traitement des contrats", ncols=100):
+    contract_rows = df[df["no_contr"] == contract]
+    processed_data = process_contract_rows(contract_rows)
+    result_data.append(processed_data)
+
+# Créer un DataFrame à partir des données traitées
+result_df = pd.DataFrame(result_data)
+
+# Enregistrer les résultats dans un fichier Excel
+result_df.to_excel('structured_data.xlsx', index=False, engine='openpyxl')
