8caaab55d797dbc54c8c3a5b799de906977f08a3
WebCresson <alexis@webcresson.com>
2023-10-27 14:06:04 +0200

    Changement de logique pour column

--- Correspondance/main.py
+++ Correspondance/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Étape 1: Charger les fichiers
 df_etablissement = pd.read_excel("Liste_Etablissements.xlsx")
-df_dsi = pd.read_excel("count_dsi_trier.xlsx")
+df_dsi = pd.read_excel("count_dsi_trier_V2.xlsx")
 
 # Création d'un dictionnaire pour faciliter la recherche
 dict_etablissement = df_etablissement.set_index('Code Regate').to_dict(orient='index')
@@ -19,10 +19,20 @@ for col in df_dsi.columns:
             if code in dict_etablissement:
                 site_traitement = dict_etablissement[code]["Site traitement Oui/Non"]
                 ouvert_status = dict_etablissement[code]["Ouvert Oui/Non"]
-                return "Non Traitement" if site_traitement == "Non" else "Correspondance", "Ouvert" if ouvert_status == "Oui" else "Fermé"
+                
+                # Détermine le résultat en fonction de site_traitement
+                if site_traitement == "Oui":
+                    result = "Correspondance / Traitement"
+                elif site_traitement == "Non":
+                    result = "Non Traitement"
+                else:
+                    result = "Correspondance"
+                
+                ouvert = "Ouvert" if ouvert_status == "Oui" else "Fermé"
+                return result, ouvert
             return "Non correspondance", ""
 
         df_dsi[result_col_name], df_dsi[ouvert_col_name] = zip(*df_dsi[col].map(apply_logic))
 
 # Étape 6: Sauvegarder les modifications dans le même fichier
-df_dsi.to_excel("count_dsi_trier_V2.xlsx", index=False)
+df_dsi.to_excel("count_dsi_trier_V2_test.xlsx", index=False)
